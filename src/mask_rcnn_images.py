a73580d3cf5fcbff50105a2578a5e53c03ac9d85
sebastian_kup <sebastian.kuppa@web.de>
2022-10-15 13:28:22 +0800

    added image segmentation with mrcnn

--- src/mask_rcnn_images.py
+++ src/mask_rcnn_images.py
@@ -14,3 +14,37 @@ parser.add_argument('-t', '--threshold', default=0.965, type=float,
                     help='score threshold for discarding detection')
 args = vars(parser.parse_args())
 
+# MASK RCNN model
+model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True, progress=True,
+                                                           num_classes=91)
+# set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# load model and set to eval mode
+model.to(device).eval()
+
+# convert image to tensor
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+image_path = args['--input']
+image = Image.open(image_path).convert('RGB')
+# keep original image for opencv operations
+orig_image = image.copy()
+
+image = transform(image)
+# add batch dimension
+image = image.unsqueeze(0).to(device)
+
+masks, boxes, labels = get_outputs(image, model, args['threshold'])
+
+result = draw_segmentation_map(image, masks, boxes, labels)
+
+# visualize image
+cv2.imshow('Segmented image', result)
+cv2.waitKey(0)
+
+# set savePath
+save_path = f"../outputs/{args['--input'].split('/')[-1].split('.')[0]}.jpg"
+cv2.imwrite(save_path, result)
